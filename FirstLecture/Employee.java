7f29507461dd07ed24641c69bea9772a3cc3bbc3
iMythms <maythams.149@gmail.com>
2023-01-26 08:37:46 +0300

    commit

---
+++ FirstLecture/Employee.java
@@ -0,0 +1,48 @@
+package FirstLecture;
+
+public class Employee {
+    private int ID;
+    private String Name;
+    private String Position;
+    private double Salary;
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public void setPosition(String Position) {
+        this.Position = Position;
+    }
+
+    public void setSalary(double Salary) {
+        this.Salary = Salary;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String Name() {
+        return Name;
+    }
+
+    public String getPosition() {
+        return Position;
+    }
+
+    public double getSalary() {
+        return Salary;
+    }
+
+    public void display() {
+        System.out.println(
+                "Name: " + Name +
+                        "\nID: " + ID +
+                        "\nPosition: " + Position +
+                        "\nSalary: " + Salary);
+    }
+}
---
+++ FirstLecture/EmployeeTest.java
@@ -0,0 +1,15 @@
+package FirstLecture;
+
+public class EmployeeTest {
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+
+        e1.setID(905127);
+        e1.setName("Mytham");
+        e1.setPosition("Developer");
+        e1.setSalary(5000);
+
+        e1.display();
+        System.out.println(e1);
+    }
+}
