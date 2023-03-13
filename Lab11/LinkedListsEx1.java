f1d8b020802a2eaee464052555708e0c1fe5f197
iMythms <maythams.149@gmail.com>
2023-03-13 14:11:10 +0300

    commit

---
+++ Lab11/LinkedListsEx1.java
@@ -0,0 +1,11 @@
+package Lab11;
+
+import java.util.LinkedList;
+
+public class LinkedListsEx1 {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<String>();
+        list.add("Salman");
+        list.add("Mohammed");
+    }
+}
